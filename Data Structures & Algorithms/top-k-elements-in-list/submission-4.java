dcd3aee2e3f35098646c9e3d392cc3ebca6255af
Aadith M Mathew <aadithmathew2017@gmail.com>
2026-08-01 01:50:51 +0530

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Arrays.sort(nums);
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        int[] result = new int[k];
+        list2.add(nums[0]);
+        if(nums.length==1){
+            list.add(list2);
+        }
+        for (int i = 1; i < nums.length; i++) {
+            if (list2.get(0) == nums[i]) {
+                list2.add(nums[i]);
+            } else {
+                list.add(new ArrayList<>(list2));
+                list2.clear();
+                list2.add(nums[i]);
+            }
+
+            if ((i == nums.length - 1) && !list2.isEmpty()) {
+                list.add(new ArrayList<>(list2));
+            }
+        }
+        list.sort((a, b) -> Integer.compare(b.size(), a.size()));
+        for(int i = 0; i<k; i++){
+            result[i] = list.get(i).get(0);
+        }
+        return result;
+    }
+}
